1e962935a74ae8064623c24d1d4dcb75c75a0f8b
Oskari Ruotsalainen <78789083+oskariruo@users.noreply.github.com>
2023-01-23 06:40:42 +0200

    app.js cleaned up and theme changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
@@ -11,24 +10,28 @@ import ADminScreen from './screens/AdminScreen';
 
 function App() {
 
-  const mdTheme = createTheme();
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
 
   return (
     <div className="App">
       <BrowserRouter>
-      <ThemeProvider theme={mdTheme}>
-            <Box sx={{ display: 'flex' }}>
+      <ThemeProvider theme={darkTheme}>
+          <Box sx={{ display: 'flex' }}>
             <List component="nav">
-            {mainListItems}
-          </List>
-            </Box>
+              {mainListItems}
+            </List>
+          </Box>
         </ThemeProvider>
 
         <Routes>
-                        <Route path="/" element={<WorkScreen />}/>
-                        <Route path="/account" element={<AccountScreen />}/>
-                        <Route path="/admin" element={<ADminScreen />}/>
-                    </Routes>
+          <Route path="/" element={<WorkScreen />}/>
+          <Route path="/account" element={<AccountScreen />}/>
+          <Route path="/admin" element={<ADminScreen />}/>
+        </Routes>
       </BrowserRouter>
     </div>
   );
